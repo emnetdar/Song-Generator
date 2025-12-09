1acb3ead4d5e3f8f71c84a699d849e0e7ffa5d47
Emnet.Dargiee <theamakata@gmail.com>
2025-12-08 22:21:42 -0600

    added simplified version of original project

---
+++ src/project.py
@@ -0,0 +1,37 @@
+
+import sys
+
+def main():
+
+    question_bank = ["What's your favorite color?", "Cats or dogs?", "Do you like the arts?", "Do you have social media?"]
+    album_bank = ["Songs in the Key of Life by Stevie Wonder", "Falsettos (2016 Broadway Cast Recording) by William Finn", 
+                "Songs about Jane by Maroon 5", "I Know I'm Funny Haha by Faye Webster"]
+    user_score = 0
+
+    input1 = input("Hello there! Wanna know what album I'd give you? (yes/no): ").lower()
+
+    if input1 == "yes":
+        print(f"Yay! Here's your first question.")
+    if input1 == "no":
+        print(f"Aw, okay. Byebye!")
+        sys.exit
+    else:
+        input(f"Sorry, I didn't catch that. Would you like to know what album I'd give you? (Y/N): ")
+
+    input2 = input(f"{question_bank[0]}").lower()
+
+    if input2 == "red" or "yellow":
+        print(f"Oh, that's one of my favorites too! Here's the next one.")
+        user_score += 1
+    if input2 == "orange" or "blue":
+        print(f"Not my fave, but I get the appeal hehe. Next one!")
+    if input2 == "green" or "purple":
+        print(f"Ew... Moving on.")
+    else:
+        input("Sorry, I don't know that one. Maybe try something basic?: ")
+
+
+
+
+if __name__ == "__main__":
+    main()
